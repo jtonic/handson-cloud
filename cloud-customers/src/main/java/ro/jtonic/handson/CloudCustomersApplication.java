f8bc90af390190d1e9537f1052cd1aa1cb67c4d1
jtonic <antonel.pazargic@gmail.com>
2016-10-27 14:28:51 +0300

    Remove incorrect service cloud configuration

--- cloud-customers/src/main/java/ro/jtonic/handson/CloudCustomersApplication.java
+++ cloud-customers/src/main/java/ro/jtonic/handson/CloudCustomersApplication.java
@@ -3,7 +3,6 @@ package ro.jtonic.handson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableConfig
 @SpringBootApplication
 public class CloudCustomersApplication {
 
