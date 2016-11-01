43ee4048b965263af74ffc3e2e6d999d2c4f179f
jtonic <antonel.pazargic@gmail.com>
2016-11-01 23:21:46 +0200

    Initial commit of the service discovery Eureka based project

---
+++ cloud-services-discovery/src/main/java/ro/jtonic/handson/cloud/discovery/CloudServicesDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package ro.jtonic.handson.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudServicesDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServicesDiscoveryApplication.class, args);
+	}
+}
